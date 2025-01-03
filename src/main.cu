893bc83712be9db32c1d2f759252c929e93a44fe
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-01-03 17:47:52 +0100

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -134,7 +134,7 @@ sharedNbody_Kernel(int numElements, float4 *bodyPos, float3 *bodySpeed)
 	int elementId = blockIdx.x * blockDim.x + threadIdx.x;
 	int sharedId = threadIdx.x;
 
-	__shared__ float4 sharedBodyPos[4096];
+	__shared__ float4 sharedBodyPos[1024];
 
 	int tiles = (numElements + 4095) / 4096;
 
